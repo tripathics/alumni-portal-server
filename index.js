eb23ad77a1d30d7efd30e7dce8345d3b61c9b57b
Chandrashekhar Tripathi <tripathics@outlook.com>
2024-03-14 19:41:59 +0530

    Move files inside src directory

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import './config/dotenv.config.js';
-import transporter from './config/nodemailer.config.js';
-import app from './config/express.config.js';
-import logger from './config/logger.config.js';
-import db from './config/db.config.js';
+import './src/config/dotenv.config.js';
+import app from './src/config/express.config.js';
+import db from './src/config/db.config.js';
+import logger from './src/config/logger.config.js';
+import transporter from './src/config/nodemailer.config.js';
 
 const PORT = process.env.PORT || 3000;
 
