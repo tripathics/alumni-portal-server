5dce91feba37c3643505c9fb0501c678c207aa07
Chandrashekhar Tripathi <tripathics@outlook.com>
2024-03-18 13:42:34 +0530

    file upload limits

--- src/middlewares/media.middleware.js
+++ src/middlewares/media.middleware.js
@@ -3,10 +3,10 @@ import storage from '../config/storage.config.js';
 
 export const updateAvatarFile = multer({
   storage,
-  limits: { fileSize: 200 * 200 * 5 },
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
 }).single('avatar');
 
 export const updateSignFile = multer({
   storage,
-  limits: { fileSize: 200 * 200 * 5 },
+  limits: { fileSize: 200 * 1024 }, // 200KB
 }).single('sign');
